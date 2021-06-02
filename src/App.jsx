04ee71b5284b3d8d8519bd981268b3d7e049a52e
Andrey Klemenko <web.klemenko@gmail.com>
2021-06-02 22:43:28 +0300

    get current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
+import { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import NavBar from './Views/NavBar';
 import MainView from './Views/MainView';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 import ContactsView from './Views/ContactsView';
+import { getCurrentUser } from './redux/auth/auth-operations';
 
-const App = () => (
-  <>
-    <NavBar />
-    <Switch>
-      <Route exact path="/" component={MainView} />
-      <Route path="/registration" component={RegisterForm} />
-      <Route path="/login" component={LoginForm} />
-      <Route path="/contacts" component={ContactsView} />
-    </Switch>
-  </>
-);
+class App extends Component {
+  componentDidMount() {
+    this.props.onRefreshUser();
+  }
+  render() {
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={MainView} />
+          <Route path="/registration" component={RegisterForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/contacts" component={ContactsView} />
+        </Switch>
+      </>
+    );
+  }
+}
 
-export default App;
+const mapDispatchToProps = {
+  onRefreshUser: getCurrentUser,
+};
+
+export default connect(null, mapDispatchToProps)(App);
